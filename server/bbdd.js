04be5391f7b28bf7cf966eac6fd456f69ee3aa8a
laurattt <lauratoro422@gmail.com>
2026-02-15 20:30:48 +0100

    ...

--- server/bbdd.js
+++ server/bbdd.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     {
         host: 'localhost',
         dialect: 'mysql',
-        logging: true,
+        logging: false,
         define: {
             timestamps: false,
             freezeTableName: true
