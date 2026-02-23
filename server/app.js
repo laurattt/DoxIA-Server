fbbcf8f652c9b36f3b3a9996b9f5882fc5cdfd68
laurattt <lauratoro422@gmail.com>
2026-02-23 16:57:01 +0100

    descomentando querySMs

--- server/app.js
+++ server/app.js
@@ -61,8 +61,8 @@ app.post('/api/usuaris/registrar', async (req, res) => {
             });
         }
 
-        //const codiGenerat = await querySms(telefon); 
-        const codiGenerat = "123456"
+        const codiGenerat = await querySms(telefon); 
+        // const codiGenerat = "123456"
 
         if (!codiGenerat) {
             return res.status(500).json({
