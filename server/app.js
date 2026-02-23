041728e5b09cf8d8384b342d55733dd27911a11b
laurattt <lauratoro422@gmail.com>
2026-02-23 19:32:25 +0100

    metodo get

--- server/app.js
+++ server/app.js
@@ -106,7 +106,7 @@ async function querySms(telefonUser) { // probar aqui?
 
     try {
         const response = await fetch(`http://192.168.1.16:8000/api/sendsms/`, {
-            method: 'POST',
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
