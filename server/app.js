0fceaa50db121cb46eba30fc8e3199262292111d
laurattt <lauratoro422@gmail.com>
2026-02-23 17:44:23 +0100

    codigenerat

--- server/app.js
+++ server/app.js
@@ -63,8 +63,8 @@ app.post('/api/usuaris/registrar', async (req, res) => {
 
         console.log("Usuario creado: ",currentUser);
 
-        const codiGenerat = await querySms(telefon);  //el api_key se solicita desde la web --> http://localhost:8000/token/
-        //const codiGenerat = "123456"
+        //const codiGenerat = await querySms(telefon);  //el api_key se solicita desde la web --> http://localhost:8000/token/
+        const codiGenerat = "123456"
 
         if (!codiGenerat) {
             return res.status(500).json({
